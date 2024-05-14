e2560edaad35c421ce733da3bfc6db1b1bb11b61
youssefash97 <youssef.ashour71@hotmail.com>
2024-05-15 00:40:27 +0100

    final push 5

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -3,14 +3,6 @@ import { startCase } from "lodash";
 import { auth } from "@clerk/nextjs";
 import { OrgControl } from "./_components/org-control";
 
-export async function generateMetaData() {
-  const { orgSlug } = auth();
-
-  return {
-    title: startCase(orgSlug || "organization"),
-  };
-}
-
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
